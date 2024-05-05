a873d5aa6fe428b530512be8e5bc26b746fcb8cd
AAndrews-1982 <altonandrews@icloud.com>
2024-05-05 17:39:10 +0000

    Fix for src file

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,11 +1,11 @@
-// src/index.js
+// Task 0 - src/index.js
 // Importing the jQuery library
 import $ from 'jquery';
 
 // Waiting on the DOM to fully load before executing the code
 $(document).ready(function() {
 // Appending a paragraph element with the text 
-    $('body').append('<p>This is paragraph 1</p>');
-    $('body').append('<p>This is paragraph 2</p>');
-    $('body').append('<p>This is paragraph 3</p>');
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
 });
