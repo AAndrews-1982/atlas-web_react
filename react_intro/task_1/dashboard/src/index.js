d1d3ce2c25a925a33248a0e1dbaa19baa4ec0c17
AAndrews-1982 <altonandrews@icloud.com>
2024-05-07 17:49:57 +0000

    Task 1 Embedding expressions and functions

---
+++ react_intro/task_1/dashboard/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Notifications from './Notifications'; // Import the Notifications component
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// Render the Notifications component in a separate div
+ReactDOM.render(
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>,
+  document.getElementById('root-notifications')
+);
